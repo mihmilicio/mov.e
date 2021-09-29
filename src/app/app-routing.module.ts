d2b44d444f5c8dbeba3a8f870009ce3f4bb9d7b7
mihmilicio <37940750+mihmilicio@users.noreply.github.com>
2021-09-29 20:16:57 -0300

    routing and header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { FormComponent } from './pages/form/form.component';
+import { ListComponent } from './pages/list/list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+      path: "",
+      component: ListComponent,
+  },
+  {
+    path: 'cadastro',
+    component: FormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
